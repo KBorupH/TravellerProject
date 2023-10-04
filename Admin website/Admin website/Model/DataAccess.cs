759012c4aa55507a81bf98f87b6757ebce35c758
JanickLarsen <janickelev@gmail.com>
2023-10-04 13:06:49 +0200

    website

---
+++ Admin website/Admin website/Model/DataAccess.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Data.SqlClient;
+using System.Text;
+
+namespace Admin_website.Model
+{
+	//ip adders 10.108.149.14
+	public class DataAccess
+	{
+		// returns connection string for linked database
+		public SqlConnection GetConnection()
+		{
+			return new SqlConnection(@"Data Source = 10.108.149.14,5432; Initial Catalog = Traveller; Trusted_Connection=true; TrustServerCertificate=true;");
+		}
+		
+	}
+}
