4bce720e4baf12463217102208fa2967da83765f
JanickLarsen <janickelev@gmail.com>
2023-10-05 13:41:41 +0200

    update on the website

--- Admin website/Admin website/Pages/Index.cshtml.cs
+++ Admin website/Admin website/Pages/Index.cshtml.cs
@@ -1,20 +1,19 @@
-﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Diagnostics;
 
 namespace Admin_website.Pages
 {
-    public class IndexModel : PageModel
+    public class indexModel : PageModel
     {
-        private readonly ILogger<IndexModel> _logger;
-
-        public IndexModel(ILogger<IndexModel> logger)
-        {
-            _logger = logger;
-        }
-
         public void OnGet()
         {
-
         }
-    }
+		public void OnPostTest()
+		{
+			NavigationManager.NavigateTo("/Worker");
+		}
+
+	}
 }
---
+++ Admin website/Admin website/Pages/WorkerPage.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Admin_website.Pages
+{
+    public class WorkerPageModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
