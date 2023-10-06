b22f47fcb9164b3f08062d5b1d2efefd2b33e2d0
JanickLarsen <janickelev@gmail.com>
2023-10-06 10:45:56 +0200

    2

---
+++ Admin website/Admin website/Pages/index.cshtml.cs
@@ -0,0 +1,24 @@
+using Admin_website.Model;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Diagnostics;
+
+namespace Admin_website.Pages
+{
+    public class indexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+		public IActionResult OnPostTest()
+		{
+			DataAccess DataAccess = new DataAccess();
+			DataAccess.test();
+
+			return RedirectToPage("WorkerPage");
+
+		}
+
+	}
+}
