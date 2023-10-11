267f6ddf91159707fdf464819401aef549de69d2
Zakabiass <141635072+Zakabiass@users.noreply.github.com>
2023-10-11 14:08:49 +0200

    train log class
    train log class

---
+++ Traveller/AdminSite/Admin website/Model/TrainLogClass.cs
@@ -0,0 +1,32 @@
+﻿using Npgsql;
+using System.Data;
+using System.Reflection;
+using Admin_website.Model;
+
+public class DatabaseAccess
+{
+    public DataTable GetDataFromTable(string tableName)
+    {
+        Admin_website.Model.DataAccess CoolMathGames = new Admin_website.Model.DataAccess();
+        NpgsqlConnection connectionString = CoolMathGames.GetConnection();
+        {
+
+            connectionString.Open();
+
+                var sqlQuery = $"SELECT * FROM {tableName}";
+                using (var command = new NpgsqlCommand(sqlQuery, connectionString))
+                {
+                    using (var reader = command.ExecuteReader())
+                    {
+                        var dataTable = new DataTable();
+                        dataTable.Load(reader);
+                        return dataTable;
+                    }
+                }
+        }
+    }
+}
+
+
+   
+
